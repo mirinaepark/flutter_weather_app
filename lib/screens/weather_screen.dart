e4effd62af9a313b3f2e2e817a0d6a739cd581df
rinae <goroweon@gmail.com>
2023-11-01 14:32:29 +0900

    1. 날씨 앱 레이아웃 2. svg package, image 적용 3. time builder package 적용 4. google font package 적용 5. intl package 로 날짜/시간 형식 변형

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({this.parseWeatherData});
-  final parseWeatherData;
 
+  final parseWeatherData;
 
   @override
   State<WeatherScreen> createState() => _WeatherScreenState();
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-
   String? cityName;
   int? temp;
+  var data = DateTime.now();
 
   @override
   void initState() {
@@ -22,7 +26,6 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   void updateData(dynamic weatherData) {
-
     double temp2 = weatherData['main']['temp'];
     temp = temp2.round();
     cityName = weatherData['name'];
@@ -31,27 +34,224 @@ class _WeatherScreenState extends State<WeatherScreen> {
     print('CityName : $cityName');
   }
 
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:mm a").format(now);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'cityName: $cityName',
-                style: const TextStyle(fontSize: 30.0),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'Temperature : $temp',
-                style: const TextStyle(fontSize: 30.0),
-              ),
-            ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        /*title: Text(''),*/
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.near_me),
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.location_searching),
+            iconSize: 30.0,
           ),
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 150.0,
+                            ),
+                            Text(
+                              'Seoul',
+                              style: GoogleFonts.lato(
+                                fontSize: 35.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                  (const Duration(minutes: 1)),
+                                  builder: (context) {
+                                    print(
+                                        'getSystemTime :  ${getSystemTime()}');
+                                    return Text(
+                                      '${getSystemTime()}',
+                                      style: GoogleFonts.lato(
+                                          fontSize: 16.0, color: Colors.white),
+                                    );
+                                  },
+                                ),
+                                Text(
+                                  DateFormat('  - EEEE, ').format(data),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                ),
+                                Text(
+                                  DateFormat('d MMM, yyy').format(data),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '18\u2103',
+                              style: GoogleFonts.lato(
+                                fontSize: 85.0,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset('svg/climacon-sun.svg',
+                                    width: 100.0, height: 100.0),
+                                const SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text(
+                                  'clear sky',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const Divider(
+                        height: 15.0,
+                        thickness: 2.0,
+                        color: Colors.white30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'AQI(대기질수)',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0, color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Image.asset(
+                                'image/bad.png',
+                                width: 37.0,
+                                height: 35.0,
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '매우나쁨',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '미세먼지',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0, color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '174.75',
+                                style: GoogleFonts.lato(
+                                    fontSize: 24.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '단위',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '초미세먼지',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0, color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '85',
+                                style: GoogleFonts.lato(
+                                    fontSize: 24.0, color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '단위',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
